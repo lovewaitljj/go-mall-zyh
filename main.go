973aaaa8fe29b6fffd0879324d3efc113e2a9608
KevinYan <klein@KleindeMacBook-Pro.local>
2024-07-07 14:25:24 +0800

    项目配置定制化和一体化打包方案

--- main.go
+++ main.go
@@ -1,17 +1,29 @@
 package main
 
 import (
+	"github.com/go-study-lab/go-mall/config"
 	"net/http"
 
 	"github.com/gin-gonic/gin"
 )
 
 func main() {
-	r := gin.Default()
-	r.GET("/ping", func(c *gin.Context) {
+	g := gin.New()
+	// TODO: 后面会把应用日志统一收集到文件， 这里根据运行环境判断, 只在dev环境下才使用gin.Logger()输出信息到控制台
+	g.Use(gin.Logger(), gin.Recovery())
+	g.GET("/ping", func(c *gin.Context) {
 		c.JSON(http.StatusOK, gin.H{
 			"message": "pong",
 		})
 	})
-	r.Run() // listen and serve on 0.0.0.0:8080 (for windows "localhost:8080")
+
+	g.GET("/config-read", func(c *gin.Context) {
+		database := config.Database
+		c.JSON(http.StatusOK, gin.H{
+			"type":     database.Type,
+			"max_life": database.MaxLifeTime,
+		})
+	})
+	g.Run(":8080") // listen and serve on 0.0.0.0:8080 (for windows "localhost:8080")
+
 }
