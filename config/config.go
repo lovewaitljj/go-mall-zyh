4e2219af0049ff6ac19ee22915ed59b4e838d740
zyh <yiheng.zhang@funplus.com>
2024-11-08 18:36:29 +0800

    up

--- config/config.go
+++ config/config.go
@@ -11,12 +11,17 @@ var (
 type appConfig struct {
 	Name string `mapstructure:"name"`
 	Env  string `mapstructure:"env"`
+	Log  struct {
+		FilePath         string `mapstructure:"path"`
+		MaxSize          int    `mapstructure:"max_size"`
+		BackUpFileMaxAge int    `mapstructure:"max_age"`
+	}
 }
 
 type databaseConfig struct {
 	Type        string        `mapstructure:"type"`
 	DSN         string        `mapstructure:"dsn"`
-	MaxOpenConn int           `mapstructure:"maxopen""`
-	MaxIdleConn int           `mapstructure:"maxidle"`
-	MaxLifeTime time.Duration `mapstructure:"maxlifetime"`
+	MaxOpenConn int           `mapstructure:"max_open""`
+	MaxIdleConn int           `mapstructure:"max_idle"`
+	MaxLifeTime time.Duration `mapstructure:"max_lifetime"`
 }
